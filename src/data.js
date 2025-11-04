7894bad6b139093bb0a181b9c1d0f3732e9c06bf
Kandiah C <kandiahc.it2023@citchennai.net>
2025-11-04 18:58:45 +0530

    make a fix in the image loading to make it fast render

--- src/data.js
+++ src/data.js
@@ -4,16 +4,16 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import MailOutlineIcon from "@mui/icons-material/MailOutline";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
-import ChatApp from "./components/assets/ChatApp.png";
-import WeatherApp from "./components/assets/WeatherApp.png";
-import LoginForm from "./components/assets/LoginForm.png";
-import RestaurantDesign from "./components/assets/RestaurantDesign.png";
-import Calculator from "./components/assets/Calculator.png";
-import UIDesign from "./components/assets/ui-design.png";
-import webDesign from "./components/assets/web-designer.png";
-import AppDesign from "./components/assets/app-design.png";
-import pdfquerychatbot from "./components/assets/PDF_query_chatbot.png"
-import bookRecky from "./components/assets/BookRecky.png"
+// import ChatApp from "./components/assets/ChatApp.png";
+// import WeatherApp from "./components/assets/WeatherApp.png";
+// import LoginForm from "./components/assets/LoginForm.png";
+// import RestaurantDesign from "./components/assets/RestaurantDesign.png";
+// import Calculator from "./components/assets/Calculator.png";
+// import UIDesign from "./components/assets/ui-design.png";
+// import webDesign from "./components/assets/web-designer.png";
+// import AppDesign from "./components/assets/app-design.png";
+// import pdfquerychatbot from "./components/assets/PDF_query_chatbot.png"
+// import bookRecky from "./components/assets/BookRecky.png"
 export const data = {
     name : "Kandiah",
     TechKnown : ["Web Designer", "Web Developer","App Developer"],
@@ -109,17 +109,17 @@ export const data = {
     ],
     skillList : [
       {
-        img: UIDesign ,
+        img: require("./components/assets/ui-design.png") ,
         heading: "UI/UX Design",
         para: "Design intuitive user interfaces with Figma, delivering user-friendly designs that enhance the overall user experience.",
       },
       {
-        img:  webDesign ,
+        img:  require("./components/assets/web-designer.png") ,
         heading: "Web Developer",
         para: "Build responsive and dynamic web applications using React, HTML,and CSS, leveraging frontend development skills to create seamless and engaging user experiences.",
       },
       {
-        img: AppDesign ,
+        img: require("./components/assets/app-design.png") ,
         heading: "App Development",
         para: "Design and Develop Intuitive, visually engaging mobile applications with user-friendly interfaces and seamless experiences.",
       },
@@ -127,43 +127,43 @@ export const data = {
     ProjectList : [
         {
           head:"BookRecky",
-          img:bookRecky,
+          img:require("./components/assets/BookRecky.png"),
           desc:"BOOKRECKY is a cross-platform React Native app combining book recommendations with social features. Users share book photos, explore picks, and build reading lists. With Zustand for state and secure auth, plus cron jobs to keep servers active, it delivers modern UI/UX and strong performance.",
           link:"https://github.com/kandiahc-it/BookReckyApp"
         },
         {
           head:"RAG Application",
-          img:pdfquerychatbot,
+          img:require("./components/assets/PDF_query_chatbot.png"),
           desc:"A RAG-based PDF chatbot that extracts and stores content in a vector database, enabling LLM-powered, context-aware answers with support for multiple PDF uploads",
           link:"https://github.com/kandiahc-it/Pdf_Querying_ChatBot"
         },
         {
           head:"ChatApp",
-          img:ChatApp,
+          img:require("./components/assets/ChatApp.png"),
           desc:"This Chat Application is a full-stack project built using the MERN stack. It enables real-time messaging with secure user authentication, clean UI, and message synchronization. The app demonstrates seamless performance, responsiveness, and a modern user experience. Perfect for showcasing skills in React, Node.js, MongoDB, and Express.",
           link:"https://github.com/kandiahc-it/mern_chat_application-onChat-"
         },
         {
           head:"Weather App",
-          img:WeatherApp,
+          img:require("./components/assets/WeatherApp.png"),
           desc:"This Weather Application is a responsive web app built using React and the OpenWeather API. It provides real-time weather updates, including temperature, humidity, and weather conditions, for any location. With a clean and intuitive user interface, this app ensures an enhanced user experience while showcasing skills in API integration, React, and responsive design.",
           link:"https://github.com/kandiahc-it/Weather-App"
         },
         {
           head:"Login Form",
-          img:LoginForm,
+          img:require("./components/assets/LoginForm.png"),
           desc:"A responsive Login and Signup Form built with React, featuring Formik for form handling, Yup for validation, and Toastify for user-friendly notifications. It ensures clean UI and real-time validation for a smooth user experience.",
           link:"https://github.com/kandiahc-it/Login-Signup-Page-formik-yup-react"
         },
         {
           head:"Restaurant App Design",
-          img:RestaurantDesign,
+          img:require("./components/assets/RestaurantDesign.png"),
           desc:"A modern restaurant app design featuring a personalized profile page, stunning dish showcases, and an easy-to-use cart for smooth ordering.",
           link:"https://github.com/kandiahc-it/Codsoft/blob/main/restaruant%20interface.fig"
         },
         {
           head:"Simple Calculator",
-          img:Calculator,
+          img:require("./components/assets/Calculator.png"),
           desc:"A clean and modern calculator UI featuring dynamic buttons and a responsive display for real-time calculation updates. Built with fundamental HTML, CSS, and JavaScript, the design ensures smooth functionality and an intuitive user experience, making it a sleek and practical tool for quick computations.",
           link:"https://github.com/kandiahc-it/calculator-project"
         }
